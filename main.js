f3ee4779f668f6f5f6e5bbd52fc4b092910b3aa4
NiyatiJaiin <niyatijain05206@gmail.com>
2025-07-23 15:45:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import './style.css';
+import products from "./api/products.json";
+import { showProductContainer } from './homeProductCards';
+
+//define a function name 'showProductContainer' that takes array of products as input
+showProductContainer(products);
+
